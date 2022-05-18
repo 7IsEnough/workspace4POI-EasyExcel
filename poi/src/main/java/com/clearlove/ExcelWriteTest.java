aa781a36248d7372434bc63cbbf2a5145bf9919c
Promise <976949689@qq.com>
2022-05-18 22:03:21 +0800

    2022年5月18日22:03:14

--- poi/src/main/java/com/clearlove/ExcelWriteTest.java
+++ poi/src/main/java/com/clearlove/ExcelWriteTest.java
@@ -2,11 +2,13 @@ package com.clearlove;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -17,7 +19,7 @@ import org.junit.Test;
  */
 public class ExcelWriteTest {
 
-  String PATH = "E:\\workspace\\workspace4POI&EasyExcel\\poi";
+  String PATH = "E:\\workspace\\workspace4POI&EasyExcel\\poi\\";
 
   @Test
   public void testWrite03() throws Exception {
@@ -46,7 +48,7 @@ public class ExcelWriteTest {
     cell22.setCellValue(time);
 
     // 生成一张表 (IO流)  03版本使用xls结尾
-    FileOutputStream fileOutputStream = new FileOutputStream(PATH + "\\Clearlove统计表03.xls");
+    FileOutputStream fileOutputStream = new FileOutputStream(PATH + "Clearlove统计表03.xls");
     // 输出
     workbook.write(fileOutputStream);
     // 关闭流
@@ -87,6 +89,96 @@ public class ExcelWriteTest {
     // 关闭流
     fileOutputStream.close();
     System.out.println("Excel生成完毕");
+  }
+
+  @Test
+  public void testWrite03BigData() throws Exception {
+    // 时间
+    long begin = System.currentTimeMillis();
+
+    // 创建一个工作簿
+    Workbook workbook = new HSSFWorkbook();
+
+    // 创建表
+    Sheet sheet = workbook.createSheet();
+
+    // 写入数据
+    for (int rowNumber = 0; rowNumber < 65536; rowNumber++) {
+      Row row = sheet.createRow(rowNumber);
+      for (int cellNum = 0; cellNum < 10; cellNum++) {
+        Cell cell = row.createCell(cellNum);
+        cell.setCellValue(cellNum);
+      }
+    }
+
+    System.out.println("over");
+    FileOutputStream fileOutputStream = new FileOutputStream(PATH + "testWrite03BigData.xls");
+    workbook.write(fileOutputStream);
+    fileOutputStream.close();
+    long end = System.currentTimeMillis();
+    System.out.println((double) (end - begin) / 1000);
+
+  }
+
+  // 耗时较长！优化 缓存
+  @Test
+  public void testWrite07BigData() throws Exception {
+    // 时间
+    long begin = System.currentTimeMillis();
+
+    // 创建一个工作簿
+    Workbook workbook = new XSSFWorkbook();
+
+    // 创建表
+    Sheet sheet = workbook.createSheet();
+
+    // 写入数据
+    for (int rowNumber = 0; rowNumber < 65536; rowNumber++) {
+      Row row = sheet.createRow(rowNumber);
+      for (int cellNum = 0; cellNum < 10; cellNum++) {
+        Cell cell = row.createCell(cellNum);
+        cell.setCellValue(cellNum);
+      }
+    }
+
+    System.out.println("over");
+    FileOutputStream fileOutputStream = new FileOutputStream(PATH + "testWrite07BigData.xlsx");
+    workbook.write(fileOutputStream);
+    fileOutputStream.close();
+    long end = System.currentTimeMillis();
+    System.out.println((double) (end - begin) / 1000);
+
+  }
+
+  @Test
+  public void testWrite07BigDataS() throws Exception {
+    // 时间
+    long begin = System.currentTimeMillis();
+
+    // 创建一个工作簿
+    Workbook workbook = new SXSSFWorkbook();
+
+    // 创建表
+    Sheet sheet = workbook.createSheet();
+
+    // 写入数据
+    for (int rowNumber = 0; rowNumber < 65536; rowNumber++) {
+      Row row = sheet.createRow(rowNumber);
+      for (int cellNum = 0; cellNum < 10; cellNum++) {
+        Cell cell = row.createCell(cellNum);
+        cell.setCellValue(cellNum);
+      }
+    }
+
+    System.out.println("over");
+    FileOutputStream fileOutputStream = new FileOutputStream(PATH + "testWrite07BigDataS.xlsx");
+    workbook.write(fileOutputStream);
+    fileOutputStream.close();
+    // 清除临时文件
+    ((SXSSFWorkbook)workbook).dispose();
+    long end = System.currentTimeMillis();
+    System.out.println((double) (end - begin) / 1000);
+
   }
 
 }
